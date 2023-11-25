c527fd061c61e85eb98682e662e102c5376477d9
chivy140820a <chivy1408@gmail.com>
2023-11-25 16:47:18 +0700

    update

--- DemoJenkinCICD/Controllers/WeatherForecastController.cs
+++ DemoJenkinCICD/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace DemoJenkinCICD.Controllers
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
+      Console.WriteLine("Hello");
       return Enumerable.Range(1, 5).Select(index => new WeatherForecast
       {
         Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
